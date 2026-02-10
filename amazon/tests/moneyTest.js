1fe28174df598279244498e6000eea5d1c2de061
Rithik Rajan <rithikadirajan@gmail.com>
2026-02-10 19:58:49 +0530

    created automated test case for formatCurrency

---
+++ amazon/tests/moneyTest.js
@@ -0,0 +1,35 @@
+import {formatCurrency} from  '../scripts/utils/money.js';
+
+
+console.log('testing suite: formatCurrency')
+console.log('basic test')
+if (formatCurrency(2095) === '20.95')
+{
+    console.log('passed');
+}
+else
+{
+    console.log('failed')
+}
+
+
+console.log('works with zeros')
+if (formatCurrency(0) === '0.00')
+{
+    console.log('passed');
+}
+else
+{
+    console.log('failed');
+}
+
+console.log('rounds to nearest cent')
+
+if (formatCurrency(2000.5) === '20.01')
+{
+    console.log('passed');
+}
+else
+{
+    console.log('failed');
+}
