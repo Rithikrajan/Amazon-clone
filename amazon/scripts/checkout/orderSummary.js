4c2fd74a58c9ab7742d7a0e0b9860f486b6a8470
Rithikrajan <rithikadriajan@gmail.com>
2026-02-19 16:53:14 +0530

    create automated tests for orderSummary

--- amazon/scripts/checkout/orderSummary.js
+++ amazon/scripts/checkout/orderSummary.js
@@ -33,6 +33,7 @@ export function renderOrderSummary()
 
         cartSummaryHTML +=
             ` <div class="cart-item-container
+                    js-cart-item-container
                     js-cart-item-container-${matchingProduct.id}">
                     <div class="delivery-date">
                     Delivery date: ${dateString}
@@ -49,15 +50,17 @@ export function renderOrderSummary()
                         <div class="product-price">
                         $${formatCurrency(matchingProduct.priceCents)}
                         </div>
-                        <div class="product-quantity">
+                        <div class="product-quantity js-cart-quantity-${matchingProduct.id}">
                         <span>
                             Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                         </span>
                         <span class="update-quantity-link link-primary">
                             Update
                         </span>
-                        <span class="delete-quantity-link link-primary 
-                        js-delete-link" data-product-id="${matchingProduct.id}">
+                        <span class="delete-quantity-link 
+                        link-primary 
+                        js-delete-link js-delete-link-${matchingProduct.id}" 
+                        data-product-id="${matchingProduct.id}">
                             Delete
                         </span>
                         </div>
@@ -131,9 +134,9 @@ export function renderOrderSummary()
         
         const container = document.querySelector(`.js-cart-item-container-${productId}`);
         container.remove();
-
+        
         renderPaymentSummary();
-        });
+        }); 
     }); 
 
     document.querySelectorAll('.js-delivery-option')
